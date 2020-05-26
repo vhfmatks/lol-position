88411958251af9d959c32723430e4e775caff2f9
unknown <vhfmatks@gmail.com>
2020-05-26 17:44:34 +0900

    img

--- src/data/posData.js
+++ src/data/posData.js
@@ -5,13 +5,13 @@ var posData = [
       img : '/img/top.png',
       description : `
       당신의 포지션은 빼박 TOP!
-      
+
       자기주도적인 성향이 강하고 도전을 두려워하지 않는 불도저.
       대담한 성격의 소유자로 항상 자신감이 넘쳐나는 자존감의 끝판왕!
       진취적인 생각과 결정력, 그리고 냉철한 판단력으로 목표 달성을 위해 가끔은 무모하리만치 이성적 사고를 하는 것이 특징입니다.
       
       ` },
-    { position : 'jg' , score : 0 , posNm : 'JUNGLE',img : '/img/jg.png',
+    { position : 'jg' , score : 0 , posNm : 'JUNGLE',img : './img/jg.png',
       description : `
       당신의 포지션은 킹메이커, Jungle!
 
@@ -19,21 +19,21 @@ var posData = [
       결단력이 있으며, 야망이 있지만 대외적으로 표현하지 않으며, 놀랄 만큼 호기심이 많지만 쓸데없는데 에너지를 낭비하는 법이 없습니다.
       마치 체스를 두듯 새로운 계획이나 전술, 그리고 대책을 세워가며 상대방 머리 위에서 수를 두며 허를 찌르는 기술로 상황을 유리하게 몰고 가는 것을 즐깁니다.
       ` },
-    { position : 'mid', score : 0 , posNm : 'MID',img : '/img/mid.png',
+    { position : 'mid', score : 0 , posNm : 'MID',img : './img/mid.png',
     description : `
     당신의 포지션은 팀의 중심이자 핵심, Mid!
 
     타고난 승부욕과 철두철미한 계획을 세우는 완벽주의자. 
     전략적인 사고와 장기적인 안목과 더불어 빠른 판단력과 정확성으로 계획을 단계별로 실행해 나감으로써 진정한 리더의 역할을 수행합니다.
     ` },
-    { position : 'ad' , score : 0 , posNm : 'AD Carry',img : '/img/ad.png',
+    { position : 'ad' , score : 0 , posNm : 'AD Carry',img : './img/ad.png',
     description : `
     당신의 포지션은 캐리의 중심, AD Carry!
     
     넘치는 카리스마와 영향력으로 라인을 압도할 수 있는 재능을 가졌습니다.
     목표를 달성하기 위해 시간과 에너지를 허투루 쓰지 않으며, 이에 필요한 업무를 정확하고 신중하게 처리합니다.
     ` },
-    { position : 'sup', score : 0 , posNm : 'Suppoter',img : '/img/sup.png',
+    { position : 'sup', score : 0 , posNm : 'Suppoter',img : './img/sup.png',
     description : `
     당신의 포지션은 생각하는 전략가, Support!
     
